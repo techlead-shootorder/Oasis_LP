8f8793144e844f93571c7e8e6a46d3805525d796
farazshootorder <faraz@shootorder.in>
2025-01-27 15:32:53 +0530

    udpated modal form z-index

--- app/(landingpages)/components/Modal/MFModallp3.js
+++ app/(landingpages)/components/Modal/MFModallp3.js
@@ -8,7 +8,7 @@ const ModalV2 = ({ isOpen, onClose, center, service, isMeta = false }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[99999]">
   
       <div className="md:bg-[#ffe9f3] w-full max-w-lg p-0 md:p-8 rounded-lg shadow-lg relative">
         <button
